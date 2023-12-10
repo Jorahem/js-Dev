0002523e18431010dddf7f235d0949879488440d
Hem Jora <107682061+hemlaxman@users.noreply.github.com>
2023-12-10 09:15:39 +0000

    Opertion config done

---
+++ 01_basic/operation.js
@@ -0,0 +1,31 @@
+// Operation
+
+let value=3;
+let negval=-value
+// console.log(negval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello ";
+let str2="Hem";
+
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1" +2  );
+// console.log(1 +"2"  );
+// console.log("1"+ 2+2 );
+// console.log(1+ 2+"2" );
+
+// console.log(+true);
+
+let num1, num2,num3
+nu1 =  num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
