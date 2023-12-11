5ac73ef2e9ec068aa3fdf3277ea87727722232ab
Hem Jora <107682061+hemlaxman@users.noreply.github.com>
2023-12-11 09:40:21 +0000

    Date and Time

---
+++ 01_basic/Date_in_js.js
@@ -0,0 +1,17 @@
+// console.log(Date());
+ let myDate = new Date();
+//  console.log(myDate.toString());
+//  console.log(myDate.toJSON());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toLocaleDateString());
+//  console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2023 , 0, 23)
+// let myCreateDate = new Date(2023 , 0, 23 ,5,3)
+let myCreateDate = new Date("2023-01-14")
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+
